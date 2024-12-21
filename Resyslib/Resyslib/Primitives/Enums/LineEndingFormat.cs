0cd9d0b2427cf73dde11f4b3ea2b9d5f038ce560
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-21 14:05:07 +0000

    add initial Resyslib code

---
+++ Resyslib/Resyslib/Primitives/Enums/LineEndingFormat.cs
@@ -0,0 +1,14 @@
+﻿
+
+// ReSharper disable InconsistentNaming
+namespace Resyslib.Primitives.Enums
+{
+    public enum LineEndingFormat
+    {
+        CR,
+        LF,
+        CR_LF,
+        LF_CR,
+        NotDetected
+    }
+}
