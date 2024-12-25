5f3aa82761e9e9279be79eda4be6cb18cee294a0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-12-25 16:33:43 +0000

    start working on Resylib IO library

---
+++ Resyslib/Resyslib.IO/Directories/DirectoryHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Resyslib.Internal.Localizations;
+
+namespace Resyslib.IO.Directories;
+
+public static class DirectoryHelper
+{
+    
+    
+    /// <summary>
+    /// Checks if a Directory is empty or not.
+    /// </summary>
+    /// <param name="directory">The directory to be searched.</param>
+    /// <returns>true if the directory is empty; returns false otherwise.</returns>
+    /// <exception cref="DirectoryNotFoundException">Thrown if the directory does not exist.</exception>
+    public static bool IsDirectoryEmpty(string directory)
+    {
+        if (Directory.Exists(directory))
+        {
+            return Directory.GetFiles(directory).Length == 0 && Directory.GetDirectories(directory).Length == 0;
+        }
+        else
+        {
+            throw new DirectoryNotFoundException(Resources.Exceptions_DirectoryNotFound.Replace("{x}", directory));
+        }
+    }
+}
---
+++ Resyslib/Resyslib.IO/Files/Concatenation/FileConcatenator.cs
@@ -0,0 +1,63 @@
+﻿/*
+    Resyslib.IO 
+    Copyright (c) 2024 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Resyslib.IO.Files
+{
+    /// <summary>
+    /// Syntactic sugar around the FileAppender class.
+    /// </summary>
+    public static class FileConcatenator
+    {
+        /// <summary>
+        /// Concatenated the contents of files in the style of the Unix Cat command.
+        /// </summary>
+        /// <param name="files">The files to be concatenated.</param>
+        /// <returns>the concatenated files as an IEnumerable of strings.</returns>
+        public static IEnumerable<string> ConcatenateFilesToEnumerable(IEnumerable<string> files)
+        {
+            FileAppender fileAppender = new FileAppender();
+            fileAppender.AppendFiles(files);
+
+            return fileAppender.ToList();
+        }
+        
+        /// <summary>
+        /// Concatenates the contents of specified files and saves it to a new file.
+        /// </summary>
+        /// <param name="filePath">The path to save the new file to.</param>
+        /// <param name="newFileName">The name of the new file to be created.</param>
+        /// <param name="files">The files to be concatenated.</param>
+        /// <exception cref="Exception">Thrown if an exception occurs when trying to save the file.</exception>
+        public static void ConcatenateFilesToNewFile(string filePath, string newFileName, IEnumerable<string> files)
+        {
+            try
+            {
+                string newFile = $"{filePath}{Path.DirectorySeparatorChar}{newFileName}";
+
+                if (filePath.Contains(newFileName) == false)
+                {
+                    File.WriteAllLines(newFile, ConcatenateFilesToEnumerable(files));
+                }
+                else
+                {
+                    File.WriteAllLines(newFileName, ConcatenateFilesToEnumerable(files));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
