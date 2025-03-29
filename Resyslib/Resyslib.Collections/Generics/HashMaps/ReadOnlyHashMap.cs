881bdc8766e7966f596d3e93b4ec20edffc35803
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-29 14:31:59 +0000

    Update ReadOnlyHashMap.cs

--- Resyslib/Resyslib.Collections/Generics/HashMaps/ReadOnlyHashMap.cs
+++ Resyslib/Resyslib.Collections/Generics/HashMaps/ReadOnlyHashMap.cs
@@ -27,6 +27,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+// ReSharper disable ClassNeverInstantiated.Global
 
 // ReSharper disable MemberCanBePrivate.Global
 
