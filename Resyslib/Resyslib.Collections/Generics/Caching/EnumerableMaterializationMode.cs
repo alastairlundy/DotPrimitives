6df16daa3670ba45088da104e69e31c242a70e7c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-10 14:49:20 +0200

    Create EnumerableMaterializationMode.cs

---
+++ Resyslib/Resyslib.Collections/Generics/Caching/EnumerableMaterializationMode.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Resyslib.Collections
+    Copyright (c) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+namespace AlastairLundy.Resyslib.Collections.Generics.Caching
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum EnumerableMaterializationMode
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Instant,
+        /// <summary>
+        /// 
+        /// </summary>
+        Lazy
+    }
+}
