78e3ea70f2abd1f91419ff42d6a506b8c21126d5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-12 15:40:45 +0200

    add license notice header

--- Resyslib/Resyslib.Collections/Generics/HashTables/GenericHashTable.cs
+++ Resyslib/Resyslib.Collections/Generics/HashTables/GenericHashTable.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+/*
+    Resyslib.Collections
+    Copyright (c) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.Linq;
