1b3ba4e2b90ee86b42d7f0df4acb1fdab77a141d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-12 17:30:21 +0200

    Update IGenericHashTable.cs

--- Resyslib/Resyslib.Collections/Generics/HashTables/IGenericHashTable.cs
+++ Resyslib/Resyslib.Collections/Generics/HashTables/IGenericHashTable.cs
@@ -19,8 +19,7 @@ namespace AlastairLundy.Resyslib.Collections.Generics.HashTables
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
-    public interface IGenericHashTable<TKey, TValue> : IEnumerable<FlexibleKeyValuePair<TKey, TValue>>,
-        ISerializable
+    public interface IGenericHashTable<TKey, TValue> : IEnumerable<FlexibleKeyValuePair<TKey, TValue>>
     {
         /// <summary>
         /// 
