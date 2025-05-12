3afa01bc46d4172d6263cc43a4f9ea857a64d4b4
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-12 10:24:16 +0200

    add GenericHashTableBucket

---
+++ Resyslib/Resyslib.Collections/Generics/HashTables/GenericHashTableBucket.cs
@@ -0,0 +1,32 @@
+﻿using AlastairLundy.Resyslib.Collections.Generics.ArrayLists;
+
+namespace AlastairLundy.Resyslib.Collections.Generics.HashTables
+{
+    internal struct GenericHashTableBucket<TKey, TValue>
+    {
+        public int BucketCode { get; }
+
+    
+        public int Size => Items.Count;
+
+        public GenericHashTableBucket(int bucketCode)
+        {
+            BucketCode = bucketCode;
+            _items = new GenericArrayList<FlexibleKeyValuePair<TKey, TValue>>();
+        }
+
+        internal void Add(FlexibleKeyValuePair<TKey, TValue> item)
+        {
+            _items.Add(item);
+        }
+
+        internal void Remove(FlexibleKeyValuePair<TKey, TValue> item)
+        {
+            _items.Remove(item);
+        }
+
+        public GenericArrayList<FlexibleKeyValuePair<TKey, TValue>> Items => _items;
+    
+        private readonly GenericArrayList<FlexibleKeyValuePair<TKey, TValue>> _items;
+    }
+}
