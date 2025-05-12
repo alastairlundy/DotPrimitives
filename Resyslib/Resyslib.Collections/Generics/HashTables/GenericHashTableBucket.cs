ba806bebb7ab7970dc4c90c0ec16d97195d27ae8
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-12 17:32:08 +0200

    add license notice

--- Resyslib/Resyslib.Collections/Generics/HashTables/GenericHashTableBucket.cs
+++ Resyslib/Resyslib.Collections/Generics/HashTables/GenericHashTableBucket.cs
@@ -1,4 +1,20 @@
-﻿using AlastairLundy.Resyslib.Collections.Generics.ArrayLists;
+﻿/*
+    Resyslib.Collections
+    Copyright (c) 2024-2025 Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using AlastairLundy.Resyslib.Collections.Generics.ArrayLists;
+
+using AlastairLundy.Resyslib.Collections.Internal.Localizations;
 
 namespace AlastairLundy.Resyslib.Collections.Generics.HashTables
 {
