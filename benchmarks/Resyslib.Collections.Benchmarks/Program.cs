57929e91bf3283df230b17d1ec27227aef7c28ef
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-01 15:50:11 +0100

    add generic array list addition benchmark

--- benchmarks/Resyslib.Collections.Benchmarks/Program.cs
+++ benchmarks/Resyslib.Collections.Benchmarks/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using BenchmarkDotNet.Running;
-
+using Resyslib.Collections.Benchmarks.Generics.ArrayLists;
 using Resyslib.Collections.Benchmarks.Generics.HashTables;
 
-BenchmarkRunner.Run<GenericInsertionBenchmark>();
+BenchmarkRunner.Run<GenericArrayListAdditionBenchmark>();
