2f4b7d022804cb9601b0fbe88450614accd0f8ff
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-26 19:18:50 +0100

    move Collections library to new repo

--- src/Resyslib.Collections/Deprecations/DeprecationMessages.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AlastairLundy.Resyslib.Collections.Deprecations;
-
-internal static class DeprecationMessages
-{
-    internal const string DeprecationV2 = "This code is deprecated and will be removed in version 2.0";
-}
