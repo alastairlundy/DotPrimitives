bf0bf9b8e9da7229cf47d0b72c6761a615156efb
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-27 12:34:34 +0100

    Update FlexKeyValuePairEnumerableSplitExtensions.cs

--- src/AlastairLundy.Primitives/Extensions/Generic/FlexibleKeyValuePairs/FlexKeyValuePairEnumerableSplitExtensions.cs
+++ src/AlastairLundy.Primitives/Extensions/Generic/FlexibleKeyValuePairs/FlexKeyValuePairEnumerableSplitExtensions.cs
@@ -9,6 +9,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+
 using AlastairLundy.Primitives.Collections;
 
 namespace AlastairLundy.Primitives.Extensions.Generic.FlexibleKeyValuePairs;
