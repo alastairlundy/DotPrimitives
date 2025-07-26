c375d05c50f0c02f4084db957d159b6b7d856192
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-26 14:55:08 +0100

    add xml doc comments to Cache IEnumerable<T> extension method

--- src/AlastairLundy.DotPrimitives.Collections/Enumerables/Cached/Extensions/EnumerableToCachedExtensions.cs
+++ src/AlastairLundy.DotPrimitives.Collections/Enumerables/Cached/Extensions/EnumerableToCachedExtensions.cs
@@ -11,15 +11,20 @@ using System.Collections.Generic;
 
 namespace AlastairLundy.DotPrimitives.Collections.Enumerables.Cached;
 
+/// <summary>
+/// 
+/// </summary>
 public static class EnumerableToCachedExtensions
 {
    /// <summary>
    /// 
    /// </summary>
-   /// <param name="source"></param>
-   /// <param name="materializationMode"></param>
-   /// <typeparam name="T"></typeparam>
-   /// <returns></returns>
+   /// <param name="source">The underlying enumerable data to be cached.</param>
+   /// <param name="mode">The desired level of materialization for the cached values,
+   /// defaults to Lazy if not provided.
+   /// </param>
+   /// <typeparam name="T">The type of elements stored in the CachedEnumerable.</typeparam>
+   /// <returns>An instantiated <see cref="CachedEnumerable{T}"/> with the specified source <see cref="IEnumerable{T}"/> and <see cref="EnumerableMaterializationMode"/>.</returns>
    public static CachedEnumerable<T> Cache<T>(this IEnumerable<T> source,
       EnumerableMaterializationMode mode = EnumerableMaterializationMode.Lazy)
    {
