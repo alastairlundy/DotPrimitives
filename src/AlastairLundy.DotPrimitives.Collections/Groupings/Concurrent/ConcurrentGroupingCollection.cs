4974ed0bbf166002283a64619d3b5622bba35812
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-29 19:06:31 +0100

    rework ConcurrentGroupingCollection

--- src/AlastairLundy.DotPrimitives.Collections/Groupings/Concurrent/ConcurrentGroupingCollection.cs
+++ src/AlastairLundy.DotPrimitives.Collections/Groupings/Concurrent/ConcurrentGroupingCollection.cs
@@ -26,7 +26,7 @@ using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Collections.Immutable;
+using System.Diagnostics.Contracts;
 
 #if NET8_0_OR_GREATER
 using System.Diagnostics.CodeAnalysis;
@@ -41,9 +41,9 @@ namespace AlastairLundy.DotPrimitives.Collections.Groupings.Concurrent;
 /// </summary>
 /// <typeparam name="TKey">The type of the grouping keys.</typeparam>
 /// <typeparam name="TElement">The type of the elements being grouped.</typeparam>
-public class ConcurrentGroupingCollection<TKey, TElement> : IGroupingCollection<TKey, TElement>, IProducerConsumerCollection<TElement>
+public class ConcurrentGroupingCollection<TKey, TElement> : IConcurrentGroupingCollection<TKey, TElement>
 {
-    private ImmutableList<TElement> _list;
+    private readonly ConcurrentStack<TElement> _stack;
 
     /// <summary>
     /// 
@@ -54,8 +54,9 @@ public class ConcurrentGroupingCollection<TKey, TElement> : IGroupingCollection<
     {
         Key = key;
         IsReadOnly = isReadOnly;
-        _list = ImmutableList<TElement>.Empty;
-        SyncRoot = key is not null ? key : _list.GetHashCode();
+        
+        _stack = new ConcurrentStack<TElement>();
+        SyncRoot = key is not null ? key : _stack.GetHashCode();
     }
 
     /// <summary>
@@ -68,151 +69,182 @@ public class ConcurrentGroupingCollection<TKey, TElement> : IGroupingCollection<
     {
         Key = key;
         IsReadOnly = isReadOnly;
-        _list = ImmutableList.CreateRange(collection);
-        SyncRoot = key is not null ? key : _list.GetHashCode();
+        _stack = new ConcurrentStack<TElement>(collection);
+        SyncRoot = key is not null ? key : _stack.GetHashCode();
     }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <returns></returns>
-    public IEnumerator<TElement> GetEnumerator() => _list.GetEnumerator();
 
     /// <summary>
     /// 
     /// </summary>
     /// <returns></returns>
-    IEnumerator IEnumerable.GetEnumerator() => _list.GetEnumerator();
+    [Pure]
+    public IGroupingCollection<TKey, TElement> ToGroupingCollection()
+    {
+    }
 
     /// <summary>
-    /// 
+    /// Retrieves an enumerator for the internal concurrent collection.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerator{TElement}"/> object that can be used to iterate through the elements in this concurrent collection.</returns>
+    public IEnumerator<TElement> GetEnumerator() => _stack.GetEnumerator();
+
+    /// <summary>
+    /// Retrieves an enumerator for the internal concurrent collection.
+    /// </summary>
+    /// <returns>An <see cref="IEnumerator{TElement}"/> object that can be used to iterate through the elements in this concurrent.</returns>
+    IEnumerator IEnumerable.GetEnumerator() => _stack.GetEnumerator();
+
+    /// <summary>
+    /// The Key associated with all elements in this Concurrent Grouping Collection.
     /// </summary>
     public TKey Key { get; }
-    
+
     /// <summary>
-    /// 
+    /// Adds an item to this concurrent grouping.
     /// </summary>
-    /// <param name="item"></param>
+    /// <param name="item">The item to add.</param>
     public void Add(TElement item)
     {
-        _list = _list.Add(item);
+        _stack.Push(item);
     }
 
     /// <summary>
-    /// 
+    /// Extracts all elements from this grouping collection into an array.
     /// </summary>
-    /// <returns></returns>
-    public TElement[] ToArray() => _list.ToArray();
+    /// <returns>An array containing all elements in this grouping.</returns>
+    public TElement[] ToArray() => _stack.ToArray();
 
     /// <summary>
-    /// 
+    /// Removes an item from this concurrent grouping and returns if it was successfully removed.
     /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
+    /// <param name="item">The item to remove.</param>
+    /// <returns>A <see cref="bool"/> value that indicates the success of the removal operation.</returns>
     public bool TryTake(
-        #if NET8_0_OR_GREATER
-[MaybeNullWhen(false)]
- #endif
+#if NET8_0_OR_GREATER
+        [MaybeNullWhen(false)]
+#endif
         out TElement item)
     {
-        TElement? removed = _list.Take(1).FirstOrDefault();
-        
-        _list = ImmutableList.CreateRange(_list.SkipWhile(x => x is not null && x.Equals(removed)));
+       return _stack.TryPop(out item);
+    }
 
-        if (removed is not null && _list.Contains(removed) == false)
+    /// <summary>
+    /// Removes all items from this concurrent grouping collection.
+    /// </summary>
+    public void Clear()
+    {
+        _stack.Clear();
+    }
+
+    /// <summary>
+    /// Determines whether this concurrent grouping collection contains the specified item.
+    /// </summary>
+    /// <param name="item">The item to search for.</param>
+    /// <returns>True if the item was found, false otherwise.</returns>
+    public bool Contains(TElement item) => _stack.Contains(item);
+
+    /// <summary>
+    /// Removes a specified element from this grouping collection.
+    /// </summary>
+    /// <remarks>
+    /// This method changes the order of the elements in the collection.
+    /// </remarks>
+    /// <param name="item">The element to be removed.</param>
+    /// <returns>true if the element was removed, false otherwise.</returns>
+    /// <exception cref="NullReferenceException">Thrown when item is null.</exception>
+    public bool Remove(TElement item)
+    {
+        int index = _stack.IndexOf(item);
+
+        if (index == _stack.Count - 1)
         {
-            item = removed;
-            return true;
+            return _stack.TryPop(out _);
         }
         else
         {
-            item = default;
-            return false;
+            int numberOfElementsToRemove = (_stack.Count - 1) - index + 1;
+            
+            IEnumerable<TElement> elementsToReAdd = _stack.Skip(index + 1).Take(numberOfElementsToRemove - 1);
+
+            for (int i = 0; i < numberOfElementsToRemove; i++)
+            {
+                _stack.TryPop(out _);
+            }
+            
+            _stack.PushRange(elementsToReAdd.ToArray());
+            
+            return _stack.Contains(item) == false;
         }
     }
 
     /// <summary>
-    /// 
+    /// Copies the elements of this concurrent grouping to the specified array starting at the specified index.
     /// </summary>
-    public void Clear() => _list = _list.Clear();
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    public bool Contains(TElement item) => _list.Contains(item);
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    public bool Remove(TElement item)
-    {
-      _list = _list.Remove(item);
-
-      return Contains(item) == false;
-    }
-
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="array"></param>
-    /// <param name="index"></param>
-    /// <exception cref="IndexOutOfRangeException"></exception>
+    /// <param name="array">The one-dimensional array that is the target of the copy.</param>
+    /// <param name="index">The zero-based integer indicating the index at which copying begins.</param>
+    /// <exception cref="IndexOutOfRangeException">Thrown if index is less than 0 or greater than the length of the target array.</exception>
     public void CopyTo(Array array, int index)
     {
-        #if NET8_0_OR_GREATER
+#if NET8_0_OR_GREATER
         ArgumentNullException.ThrowIfNull(array, nameof(array));
-        #endif
-        
-        if(index < 0 || index > array.Length)
+#endif
+
+        if (index < 0 || index > array.Length)
             throw new IndexOutOfRangeException();
-        
-        for (int i = 0; i < array.Length; ++i)
+
+        int internalIndex = index;
+        foreach (TElement element in _stack)
         {
-            array.SetValue(_list[i] , index + i);
+            array.SetValue(element, internalIndex);
+            internalIndex++;
         }
     }
 
     /// <summary>
-    /// 
+    /// Gets a value indicating whether this concurrent grouping collection is synchronized.
     /// </summary>
-    public bool IsSynchronized => ((ICollection)_list).IsSynchronized;
-    
+    public bool IsSynchronized => ((ICollection)_stack).IsSynchronized;
+
     /// <summary>
     /// 
     /// </summary>
     public object SyncRoot { get; }
     
     /// <summary>
-    /// 
+    /// The number of elements in this concurrent grouping collection.
     /// </summary>
-    public int Count => _list.Count;
+    public int Count => _stack.Count;
 
     /// <summary>
-    /// 
+    /// Gets a value indicating whether this collection is read only or not.
     /// </summary>
     public bool IsReadOnly { get; }
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="array"></param>
-    /// <param name="index"></param>
-    public void CopyTo(TElement[] array, int index) => _list.CopyTo(array, index);
 
     /// <summary>
-    /// 
+    /// Copies the elements of this concurrent grouping collection to the specified array.
     /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
+    /// <param name="array">The target array.</param>
+    /// <param name="index">The zero-based index at which to begin copying elements.</param>
+    /// <exception cref="System.NullReferenceException">Thrown when either the array or index is null.</exception>
+    public void CopyTo(TElement[] array, int index) => _stack.CopyTo(array, index);
+
+    /// <summary>
+    /// Attempts to add an item to this concurrent grouping.
+    /// </summary>
+    /// <param name="item">The item to add.</param>
+    /// <returns>True if the item was successfully added, false otherwise.</returns>
     public bool TryAdd(TElement item)
     {
-        Add(item);
+        lock (_stack)
+        {
+            Add(item);
+        }
+        
+        TElement? last = _stack.LastOrDefault();
 
-        return _list.Contains(item);
+        if (last is not null && last.Equals(item))
+            return true;
+        
+        return _stack.Contains(item);
     }
 }
