4ad3f15b7632f21c776126b59c69649073bda116
Alastair Lundy <alastairlundy@noreply.codeberg.org>
2025-12-19 18:22:46 +0000

    remove DotPrimitives.Meta

--- src/DotPrimitives.Meta/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Global using directives
-
-global using System;
-global using System.ComponentModel.DataAnnotations;
-global using System.Runtime.InteropServices;
-global using AlastairLundy.DotPrimitives.Meta.Internals.Localizations;
