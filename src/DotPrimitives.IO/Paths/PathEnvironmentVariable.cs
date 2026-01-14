ea244f08d2a9406fd71beb7152c950e86f03c37d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-14 00:32:49 +0000

    fix formatting

--- src/DotPrimitives.IO/Paths/PathEnvironmentVariable.cs
+++ src/DotPrimitives.IO/Paths/PathEnvironmentVariable.cs
@@ -115,20 +115,20 @@ public static class PathEnvironmentVariable
         if (OperatingSystem.IsWindows())
         {
             return Environment
-                .GetEnvironmentVariable("PATHEXT")
-                ?.Split(PathContentsSeparatorChar, StringSplitOptions.RemoveEmptyEntries)
-                .Where(p => !string.IsNullOrWhiteSpace(p))
-                .Select(x =>
-                {
-                    x = x.Trim();
-                    x = x.Trim('"');
-                    if (!x.StartsWith('.'))
-                        x = x.Insert(0, ".");
+                       .GetEnvironmentVariable("PATHEXT")
+                       ?.Split(PathContentsSeparatorChar, StringSplitOptions.RemoveEmptyEntries)
+                       .Where(p => !string.IsNullOrWhiteSpace(p))
+                       .Select(x =>
+                       {
+                           x = x.Trim();
+                           x = x.Trim('"');
+                           if (!x.StartsWith('.'))
+                               x = x.Insert(0, ".");
 
-                    return x;
-                })
-                .Distinct(StringComparer.OrdinalIgnoreCase)
-                 ?? [".COM", ".EXE", ".BAT", ".CMD"];
+                           return x;
+                       })
+                       .Distinct(StringComparer.OrdinalIgnoreCase)
+                   ?? [".COM", ".EXE", ".BAT", ".CMD"];
         }
 
         return [""];
@@ -145,5 +145,5 @@ public static class PathEnvironmentVariable
     /// or a fallback to commonly used extensions if the variable is unset. Returns one file extension of "" on non-Windows systems.
     /// </returns>
     public static string[] GetPathFileExtensions() 
-        => OperatingSystem.IsWindows() ? EnumerateFileExtensions().ToArray() : [""];
+        => EnumerateFileExtensions().ToArray();
 }
