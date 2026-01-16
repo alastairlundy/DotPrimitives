73b07eacb65ecc1fa242e19828a24ece9c23f553
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-16 19:35:10 +0000

    add ProcessWrapper helper

---
+++ src/DotPrimitives/Internals/Helpers/ProcessWrapper.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotPrimitives.Internals.Helpers;
+
+internal class ProcessWrapper : Process
+{
+    internal ProcessWrapper(ProcessStartInfo startInfo)
+    {
+        EnableRaisingEvents = true;
+        Exited += OnExited;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+        startInfo.WorkingDirectory = Environment.CurrentDirectory;
+    }
+
+    private void OnExited(object sender, EventArgs e)
+    {
+        ExitCode = base.ExitCode;
+    }
+
+    internal bool HasStarted { get; private set; }
+    
+    internal new int ExitCode { get; private set; }
+    
+    internal new bool Start()
+    {
+        bool success = base.Start();
+        
+        HasStarted = success;
+        return success;
+    }
+
+    internal async Task<(string standardOut, string standardError)> WaitForBufferedExitAsync(
+        CancellationToken cancellationToken)
+    {
+        Task<string> standardOut =  StandardOutput.ReadToEndAsync(cancellationToken);
+        Task<string> standardError = StandardError.ReadToEndAsync(cancellationToken);
+
+        Task waitForExit = this.WaitForExitAsync(cancellationToken);
+        
+        await Task.WhenAll([standardOut, standardError, 
+            waitForExit]);
+        
+        return (standardOut.Result, standardError.Result);
+    }
+}
