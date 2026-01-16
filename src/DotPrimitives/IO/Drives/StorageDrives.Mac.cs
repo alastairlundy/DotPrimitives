3b653b5bc7151b029c6c6416f7f3333cc3c695eb
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-16 19:36:11 +0000

    add macOS StorageDrive implementation

--- src/DotPrimitives/IO/Drives/StorageDrives.Mac.cs
+++ src/DotPrimitives/IO/Drives/StorageDrives.Mac.cs
@@ -1,11 +1,42 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using DotPrimitives.Internals.Helpers;
 
 namespace DotPrimitives.IO.Drives;
 
 public static partial class StorageDrives
 {
+    [SupportedOSPlatform("macos")]
     private static IEnumerable<DriveInfo> EnumeratePhysicalDrivesMac()
     {
-       
+        if(!OperatingSystem.IsMacOS() && !OperatingSystem.IsMacCatalyst())
+            throw new PlatformNotSupportedException(Resources.Exceptions_PlatformNotSupported_RequiresOs.Replace("{targetOs}", "MacOS"));
+        
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "/usr/bin/diskutil`",
+            Arguments = "list physical | grep '^\\/dev\\/disk'"
+        };
+        
+        ProcessWrapper wrapper = new ProcessWrapper(startInfo);
+        
+        wrapper.Start();
+
+        Task<(string standardOut, string standardError)> resultsTask = wrapper.WaitForBufferedExitAsync(CancellationToken.None);
+
+        resultsTask.Wait();
+        
+        string[] lines = resultsTask.Result.standardOut.Split(Environment.NewLine)
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        foreach (string line in lines)
+        {
+            DriveInfo drive = new DriveInfo(line);
+            
+            yield return drive;
+        }
     }
 
     private static IEnumerable<DriveInfo> EnumerateLogicalDrivesMac()
@@ -13,7 +44,6 @@ public static partial class StorageDrives
         return DriveInfo.GetDrives()
             .Where(d => d.IsReady)
             .Where(d => !d.DriveType.HasFlag(DriveType.Unknown))
-            .Where(d => !d.DriveType.HasFlag(DriveType.NoRootDirectory) && !d.DriveType.HasFlag(DriveType.Ram))
             .Where(d =>
             {
                 try
