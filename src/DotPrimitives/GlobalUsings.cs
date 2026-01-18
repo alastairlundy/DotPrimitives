fc867e2548bd8209b11f8d94d025d9dbeda6dce0
Alastair Lundy <alastairlundy@noreply.codeberg.org>
2026-01-18 12:53:38 +0000

    add ExceptionThrower

--- src/DotPrimitives/GlobalUsings.cs
+++ src/DotPrimitives/GlobalUsings.cs
@@ -5,5 +5,7 @@ global using System.Diagnostics.Contracts;
 global using System.Linq;
 global using System.Runtime.Versioning;
 global using System.Text;
+global using DotPrimitives.Annotations;
+global using DotPrimitives.Exceptions;
 global using DotPrimitives.Internals.Helpers;
 global using DotPrimitives.Internals.Localizations;
