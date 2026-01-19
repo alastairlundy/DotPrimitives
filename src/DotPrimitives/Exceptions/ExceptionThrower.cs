9185fabb11bee9e13796a0680e3d19b6c0e3b913
Alastair Lundy <alastairlundy@noreply.codeberg.org>
2026-01-19 13:00:30 +0000

    update copyright notices for 2026

--- src/DotPrimitives/Exceptions/ExceptionThrower.cs
+++ src/DotPrimitives/Exceptions/ExceptionThrower.cs
@@ -1,7 +1,7 @@
 ﻿/*
     MIT License
    
-    Copyright (c) 2025-2026 Alastair Lundy
+    Copyright (c) 2025-2026-2026 Alastair Lundy
    
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
