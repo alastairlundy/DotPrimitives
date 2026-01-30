50e8ca804b625ba07caeadcba11627375251b234
Alastair Lundy <alastairlundy@noreply.codeberg.org>
2026-01-30 19:53:05 +0000

    add initial Drive Detector tests

---
+++ tests/DotPrimitives.Tests/IO/Drives/StorageDriveDetectorTests.cs
@@ -0,0 +1,31 @@
+﻿using DotPrimitives.IO.Drives;
+
+namespace DotPrimitives.Tests.IO.Drives;
+
+public class StorageDriveDetectorTests
+{
+    private readonly IStorageDriveDetector _storageDriveDetector;
+
+    public StorageDriveDetectorTests()
+    {
+        _storageDriveDetector = StorageDrives.Shared;
+    }
+    
+    [Test]
+    public async Task LogicalDrives_NotEmpty()
+    {
+        IEnumerable<DriveInfo> actual = _storageDriveDetector.EnumerateLogicalDrives();
+
+        await Assert.That(actual)
+            .IsNotEmpty();
+    }
+    
+    [Test]
+    public async Task PhysicalDrives_NotEmpty()
+    {
+        IEnumerable<DriveInfo> actual = _storageDriveDetector.EnumeratePhysicalDrives();
+
+        await Assert.That(actual)
+            .IsNotEmpty();
+    }
+}
