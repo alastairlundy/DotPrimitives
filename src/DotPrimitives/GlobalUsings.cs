b75e887cbed03ea13da33e7133f79947af539186
Alastair Lundy <alastairlundy@noreply.codeberg.org>
2026-02-01 13:03:02 +0000

    remove Deprecation Attribute

--- src/DotPrimitives/GlobalUsings.cs
+++ src/DotPrimitives/GlobalUsings.cs
@@ -5,6 +5,5 @@ global using System.Diagnostics.Contracts;
 global using System.Linq;
 global using System.Runtime.Versioning;
 global using System.Text;
-global using DotPrimitives.Annotations;
 global using DotPrimitives.Internals.Helpers;
 global using DotPrimitives.Internals.Localizations;
